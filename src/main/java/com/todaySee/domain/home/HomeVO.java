45de3afd9ace2e682aacb973447cff082c288a9a
HJH413 <91816743+HJH413@users.noreply.github.com>
2022-05-13 15:38:59 +0900

    Home error fix
    vo 파일에
    @Data
    @Table
    @Entity
    
    @Id
        Integer testHome;
    추가

--- src/main/java/com/todaySee/domain/home/HomeVO.java
+++ src/main/java/com/todaySee/domain/home/HomeVO.java
@@ -1,5 +1,16 @@
 package com.todaySee.domain.home;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Table
+@Entity
 public class HomeVO {
 
+    @Id
+    Integer testHome;
 }
