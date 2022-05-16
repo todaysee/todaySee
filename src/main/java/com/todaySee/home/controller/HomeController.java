5b80d3016403cec29f52b3d5a46aade5cc680407
irlo17 <iszrlo17@gmail.com>
2022-05-16 16:06:11 +0900

    homecontroller 내용추가 homeindex, homelist 프론트 수정
    homecontroller에 python과 연결하는 소켓 코드 추가
    homeindex 프론트 정리,  homeList 카테고리별 페이지 추가 및 프론트 수정

--- src/main/java/com/todaySee/home/controller/HomeController.java
+++ src/main/java/com/todaySee/home/controller/HomeController.java
@@ -3,6 +3,16 @@ package com.todaySee.home.controller;
 
 
 import com.todaySee.home.service.HomeService;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,17 +32,106 @@ public class HomeController {
         return "/index2";
     }
 
-    //홈 페이지
+    /** 홈 페이지
+     * 	- 로그인 후 회원의 정보를 토대로 추천 알고리즘을 돌리고 예상 평점이 높은 영화를 추려서 홈 페이지에 출력
+     * @param	로그인 세션
+     * @return	추천 알고리즘을 통한 추천 영화 출력
+     */
     @GetMapping("/")
-    public String homeIndex() {
+    public String homeIndex(Model m) {
+    	
+		// 소켓을 선언
+        try (Socket client = new Socket()) {
+        	
+        	// 소켓에 접속하기 위한 접속 정보를 선언한다.
+            InetSocketAddress ipep = new InetSocketAddress("localhost", 9001);
+            
+            // 소켓 접속
+            client.connect(ipep);
+            
+            // 소켓이 접속이 완료되면 inputstream과 outputstream을 받는다.
+            try (OutputStream sender = client.getOutputStream();) {
+            	System.out.println("전송시작");
+            	
+            	// 회원의 아이디를 보낼거니까 int -> String으로 형변환
+                String msg = String.valueOf(7);
+                
+                // string -> byte 배열 형변환
+                byte[] data = msg.getBytes();
+                
+                // ByteBuffer를 통해 데이터 길이를 byte형식으로 변환한다.
+                ByteBuffer b = ByteBuffer.allocate(4);
+                
+                // byte포멧은 little 엔디언이다.
+                b.order(ByteOrder.LITTLE_ENDIAN);
+                
+                b.putInt(data.length);
+ 
+                // 데이터 길이 전송
+                sender.write(b.array(), 0, 4);
+              
+                // 데이터 전송
+                sender.write(data);
+                data = new byte[4];
+
+                
+                // 한글깨짐 방지를 위해 BufferedReader로 받아줌
+                InputStream receiver = client.getInputStream();
+                
+                BufferedReader reader = new BufferedReader(new InputStreamReader(receiver, "UTF-8"));
+                String message = "";
+                
+                while((message = reader.readLine()) != null) {
+                	
+                	System.out.println("메시지 확인(1)"+message);
+                	System.out.println("메시지 확인(2)"+message);
+                	
+//                	message = message.trim().replace("", " ");
+                }
+                
+                m.addAttribute("test",message);
+               
+                
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    	
         return "/home/homeIndex";
     }
 
-    //목록 페이지
-    @GetMapping("/list")
-    public String homeList() {
-        return "/home/homeList";
+    /** 검색 결과 페이지 - 콘텐츠
+     * @return
+     */
+    @GetMapping("/search/content")
+    public String homeList_content() {
+        return "/home/homeList_content";
     }
+    
+    /** 검색 결과 페이지 - 인물
+     * @return
+     */
+    @GetMapping("/search/person")
+    public String homeList_person() {
+    	return "/home/homeList_person";
+    }
+
+    /** 검색 결과 페이지 - 즐겨찾기
+     * @return
+     */
+    @GetMapping("/search/bookmark")
+    public String homeList_bookmark() {
+    	return "/home/homeList_bookmark";
+    }
+    
+    /** 검색 결과 페이지 - 커뮤니티
+     * @return
+     */
+    @GetMapping("/search/community")
+    public String homeList_community() {
+    	return "/home/homeList_community";
+    }
+    
 
     //상세 페이지
     @GetMapping("/details")
