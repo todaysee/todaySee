66c7eae384aab059f0f80c8413bcf9fdc26ce579
ksoyeon <100837143+ksoyeon@users.noreply.github.com>
2022-05-20 18:45:54 +0900

    영상 상세 Back-end 파일생성

---
+++ src/main/java/com/todaySee/home/controller/DetailsController.java
@@ -0,0 +1,11 @@
+package com.todaySee.home.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DetailsController {
+
+    
+
+}
