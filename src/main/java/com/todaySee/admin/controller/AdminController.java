719695a3ea27b6febf1eedb97090a6673cb083ab
realtim1 <99743414+realtim1@users.noreply.github.com>
2022-05-20 10:44:38 +0900

    이식중
    이식중

--- src/main/java/com/todaySee/admin/controller/AdminController.java
+++ src/main/java/com/todaySee/admin/controller/AdminController.java
@@ -1,29 +1,78 @@
 package com.todaySee.admin.controller;
 
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class AdminController {
 
+	
+//	@Autowired
+//	AdminService s;
+	
+	
 	@GetMapping("/admin")
-	public String adminMain() {
-		return "admin/adminIndex";
+	public String adminChart() {
+		return "admin/movieChart";
+	}
+	
+	@GetMapping("/admin/userChart")
+	public String adminChart2() {
+		return "admin/userChart";
+	}
+	
+//	@GetMapping("/admin/userList")
+//	public String userList(Model m, test1 t) {
+//	m.addAttribute("userList",s.getUserList(t));
+//		
+//		return "admin/userList";
+//	}
+	
+	@GetMapping("/movieAdmin")
+	public String movieAdmin() {
+		return "admin/movieAdmin";
 	}
 
-    @GetMapping("/image")
-    public String image(Model m) {
-
+    @RequestMapping("/image")
+    public void image(Model m) {
         System.out.println("image 페이지 접속");
-        return "admin/image";
     }
     
-    @GetMapping("/adminTable")
+    @GetMapping("/userReport")
     public String adminTable() {
-    	return "admin/adminTable";
+    	return "admin/userReport";
     }
+//	@GetMapping("/admin/userList/{userNumber}")
+//	@ResponseBody
+//	public test1 userList2(Model m, test1 t, @PathVariable Integer userNumber) throws Exception {
+//	
+//		test1 addList = s.getUser(userNumber);
+//
+//		return addList;
+//	}
+//	
+    
+
+	@GetMapping("/testReport")
+	public String testReport() {
+		return "/admin/testReport";
+	}
+
+
+
+
+
 }
