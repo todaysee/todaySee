eaf0de6ba3319c90b84df330ddd8c994416c7f46
HJH413 <91816743+HJH413@users.noreply.github.com>
2022-05-20 17:33:15 +0900

    Ajax 테스트 버젼 경로 수정 및 매칭
    기존에 만들어 놓았던 ajax를 현재 domain에 맞춰서 정리함.

--- etc/Ajax/persistence/AjaxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todaySee.Ajax.persistence;
-
-import com.todaySee.Ajax.domain.AjaxVO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AjaxRepository extends JpaRepository<AjaxVO, Integer> {
-
-}
