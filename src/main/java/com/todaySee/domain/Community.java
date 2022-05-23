6114a8ac89af3853b69484633af2e7e819fa5d29
JugglingQuokka <100835609+JugglingQuokka@users.noreply.github.com>
2022-05-23 17:11:56 +0900

    ChatRoom, Community 도메인 수정
    낙타법 표시로 바꿈

--- src/main/java/com/todaySee/domain/Community.java
+++ src/main/java/com/todaySee/domain/Community.java
@@ -13,18 +13,22 @@ public class Community {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer community_number;
+    @Column(name="community_number")
+    Integer communityNumber;
 
-    @Column(length = 1000)
-    String community_title;
+    @Column(length = 1000, name="community_title")
+    String communityTitle;
 
     @CreationTimestamp
     @Temporal(TemporalType.DATE)
-    Date community_date;
-    @Column(length = 3000)
-    String community_content;
-    Integer community_like; // 게시글 좋아요
-    Integer community_hits; // 게시글 조회수
+    @Column(name="community_date")
+    Date communityDate;
+    @Column(length = 3000, name="community_content")
+    String communityContent;
+    @Column(name="community_like")
+    Integer communityLike; // 게시글 좋아요
+    @Column(name="community_hits")
+    Integer communityHits; // 게시글 조회수
 
     @ManyToOne
     @JoinColumn(name="user_number")
