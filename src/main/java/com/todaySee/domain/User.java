3dc2cafb4086641ae7ba5af3d9d9d8ee41378001
moonstar <munnemiroh.haejin@gmail.com>
2022-05-25 10:09:06 +0900

    5/25 회원가입 불필요한 파일 삭제 및 수정 2(최종)
    5/25 회원가입 불필요한 파일 삭제 및 수정

--- src/main/java/com/todaySee/domain/User.java
+++ src/main/java/com/todaySee/domain/User.java
@@ -4,9 +4,7 @@ import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+
 
 import java.util.Date;
 import java.util.List;
